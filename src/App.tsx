3bebe4915f87894f8366adb8ddcebe142e9dde0b
João Lucas <joaolucas.psantana@gmail.com>
2025-07-27 15:54:21 -0300

    fix: loginverify

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const router = createBrowserRouter([
 
   // Rota de Layout: envolve as outras rotas
   {
-    // loader: verifyLoggedIn,
+    loader: verifyLoggedIn,
     element: <MainLayout />,
     children: routesWithLayout,
   },
